3b4d2b0262e251252544268d8a0a263feca43a29
canar <chris.lvphung@gmail.com>
2017-09-20 19:07:48 +0200

    corr ustoa

--- src/ft_ustoa.c
+++ src/ft_ustoa.c
@@ -16,10 +16,8 @@ char	*ft_ustoa(uint16_t s, char *buffer, int base)
 {
 	int			i;
 	char		str[2 * 8 + 2];
-	int			neg;
 	uint16_t	tmp;
 
-	neg = 0;
 	ft_bzero(str, 2 * 8 + 2);
 	i = 0;
 	tmp = 0;
@@ -31,8 +29,6 @@ char	*ft_ustoa(uint16_t s, char *buffer, int base)
 	}
 	tmp = s % base;
 	str[i++] = (s % base >= 10) ? (tmp - 10 + 'a') : (tmp + '0');
-	if (neg == 1)
-		str[i++] = '-';
 	str[i] = '\0';
 	return (ft_strcpy(buffer, ft_strrev(str)));
 }
